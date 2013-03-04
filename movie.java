6384ecb1159127eb6adacf4f02112c2fae21b012
reemdiab <rdiab54@gmail.com>
2013-03-04 17:19:10 +0200

    Create movie.java

---
+++ movie.java
@@ -0,0 +1,67 @@
+
+public class Movie {
+  private String backdrop_path;
+	private String id;
+	private String original_title;
+	private String release_date;
+	private String poster_path;
+	private String title;
+	private double vote_average;
+	private int vote_count;
+	
+	
+	
+	public String getBackdrop_path() {
+		return backdrop_path;
+	}
+	public void setBackdrop_path(String backdrop_path) {
+		this.backdrop_path = backdrop_path;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getOriginal_title() {
+		return original_title;
+	}
+	public void setOriginal_title(String original_title) {
+		this.original_title = original_title;
+	}
+	public String getRelease_date() {
+		return release_date;
+	}
+	public void setRelease_date(String release_date) {
+		this.release_date = release_date;
+	}
+	public String getPoster_path() {
+		return poster_path;
+	}
+	public void setPoster_path(String poster_path) {
+		this.poster_path = poster_path;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getVote_average() {
+		return vote_average;
+	}
+	public void setVote_average(double vote_average) {
+		this.vote_average = vote_average;
+	}
+	public int getVote_count() {
+		return vote_count;
+	}
+	public void setVote_count(int vote_count) {
+		this.vote_count = vote_count;
+	}
+	
+	
+	
+	
+
+}
